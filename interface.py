f53bedd2181a2f808d08c5fc2ed0001a1eb6cad2
PhilippKeil <philippkeil95@gmail.com>
2014-01-05 15:38:26 +0100

    Initial setup

--- interface.py
+++ interface.py
@@ -1,8 +1,9 @@
 import sys
+import os
 import copy
 import ConfigParser
-from PyQt4 import QtGui
-from PyQt4 import QtCore
+import glob
+from PyQt4 import QtGui, QtCore, uic
 
 from main import Game
 
@@ -12,7 +13,8 @@ config = ConfigParser.RawConfigParser()
 config.read(config_file)
 file_locations = {'textures': config.get('locations', 'textures'),
                   'sounds': config.get('locations', 'sounds'),
-                  'levels': config.get('locations', 'levels')}
+                  'levels': config.get('locations', 'levels'),
+                  'ui': config.get('locations', 'ui')}
 
 defaults = {'obstacle_brush': QtCore.Qt.SolidPattern,
             'obstacle_brush_color': QtCore.Qt.black,
@@ -79,21 +81,57 @@ class Window(QtGui.QMainWindow):
         # Initialize the window
         super(Window, self).__init__()
 
-        self.graphics = 'low'
+        self.main_frame = uic.loadUi(os.path.dirname(__file__) + file_locations['ui'] + 'main_menu.ui')
+        self.settings_frame = uic.loadUi(os.path.dirname(__file__) + file_locations['ui'] + 'settings_menu.ui')
+        self.pre_game_frame = uic.loadUi(os.path.dirname(__file__) + file_locations['ui'] + 'pre_game_menu.ui')
 
-        # Create a canvas for the game to run inside
-        self.game_canvas = GameCanvas(self, self.graphics)
-        self.game_canvas.setFrameStyle(QtGui.QFrame.Box)
-        self.game_canvas.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.widget_stack = QtGui.QStackedWidget()
+        self.widget_stack.addWidget(self.main_frame)      # 0
+        self.widget_stack.addWidget(self.settings_frame)  # 1
+        self.widget_stack.addWidget(self.pre_game_frame)  # 2
 
-        # Show the Window
+        self.main_frame.play_button.clicked.connect(lambda: self.widget_stack.setCurrentIndex(2))
+        self.main_frame.settings_menu_button.clicked.connect(lambda: self.widget_stack.setCurrentIndex(1))
+        self.settings_frame.back_button.clicked.connect(lambda: self.widget_stack.setCurrentIndex(0))
+
+        self.pre_game_frame.back_button.clicked.connect(lambda: self.widget_stack.setCurrentIndex(0))
+        self.pre_game_frame.add_player_button.clicked.connect(self.add_player)
+        self.pre_game_frame.remove_player_button.clicked.connect(self.remove_player)
+
+        self.setCentralWidget(self.widget_stack)
+
+        self.setWindowTitle('SHOOTER')
         self.show()
-        # Set the game Canvas to be as big as possible
-        self.game_canvas.setFixedSize(self.height(), self.height())
 
-        self.setCentralWidget(self.game_canvas)
+    def add_player(self):
+        highest_player_id = self.pre_game_frame.player_tabs.count()
+        new_player_id = highest_player_id + 1
+        self.pre_game_frame.player_tabs.addTab(PlayerTab(new_player_id), 'PLAYER %s' % str(new_player_id))
 
-        self.setWindowTitle('Shooter')
+    def remove_player(self):
+        self.pre_game_frame.player_tabs.removeTab(self.pre_game_frame.player_tabs.count() - 1)
+
+
+class PlayerTab(QtGui.QWidget):
+    def __init__(self, player_id):
+        QtGui.QWidget.__init__(self)
+
+        self.player_label = QtGui.QLabel('PLAYER %s' % str(player_id))
+        self.key_list_widget = QtGui.QListWidget()
+
+        self.populate_key_list_view(player_id)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addStretch()
+        vbox.addWidget(self.player_label)
+        vbox.addWidget(self.key_list_widget)
+
+        self.setLayout(vbox)
+
+    def populate_key_list_view(self, player_id):
+        curr_key_dict = player_key_setup[player_id - 1]
+        for key in curr_key_dict:
+            self.key_list_widget.addItem('%s : %s' % (key, str(curr_key_dict[key])))
 
 
 class GameCanvas(QtGui.QFrame):
